f9304584d52e82148ecdb7de35c4c5cbaa791ea8
Anabel Emebo <emeboanabel@gmail.com>
2022-03-10 15:41:49 -0800

    Sizecc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,13 @@ int main(void)
 	char acharacter;
 	int ainteger;
 	long along;
-	long long alonglong;
+	long long alonng;
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));		printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonng));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
