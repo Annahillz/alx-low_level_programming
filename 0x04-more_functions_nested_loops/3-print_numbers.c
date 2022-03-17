29f8e2a4f17e1dba7cd5c5364130036c739c5831
Anabel Emebo <emeboanabel@gmail.com>
2022-03-17 12:25:08 -0700

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,14 +6,15 @@
  *
  * Return: 0 for success
  */
+
 void print_numbers(void)
 {
-	int i;
+	int n;
 
-	for (i = 0; i <= 9; i++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(i + '0');
+		_putchar(n + '0');
 	}
-		_putchar('/n');
+		_putchar('\n');
 
 }
