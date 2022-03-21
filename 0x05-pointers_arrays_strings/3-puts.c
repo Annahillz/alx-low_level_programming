c740f4787f081e9dd219f7f713241d43b2a7a94e
Anabel Emebo <emeboanabel@gmail.com>
2022-03-21 10:17:09 -0700

    this function prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - This function prints a string
+ * to the standard output
+ * @str: pointer with the address value
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	str;
+
+	while (str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+		_putchar(0);
+}
