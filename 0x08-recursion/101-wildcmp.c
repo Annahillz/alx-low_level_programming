8a2894fcd367f4b4da065516dedac3faa1d46257
Anabel Emebo <emeboanabel@gmail.com>
2022-03-30 15:55:24 -0700

    compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -31,6 +31,7 @@ if (s[i] == '*')
 	return (checkLast(s, i + 1));
 else if (s[i] == '\0')
 	return (1);
+
 return (0);
 }
 
@@ -46,7 +47,8 @@ return (0);
 
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
-if (s1[a] != '\0')
+
+	if (s1[a] != '\0')
 {
 	if (s2[b] == '\0')
 		return (0);
@@ -66,10 +68,13 @@ if (s1[a] != '\0')
 
 	if (wildUsed == -1)
 		return (0);
+
 	return (checker(s1, s2, a, wildUsed, wildUsed));
+
 }
 if (s2[b] != '\0')
-
 	return (checkLast(s2, b));
+
 return (1);
+
 }
