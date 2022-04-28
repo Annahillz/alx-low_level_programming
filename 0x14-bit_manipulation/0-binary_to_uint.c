041588c6053505536f80138522730424e9a8d2ba
Anabel Emebo <emeboanabel@gmail.com>
2022-04-28 15:00:45 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int n;
 
+	n = 0;
 	if (!b)
 		return (0);
 
@@ -22,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; b[i] != '\0'; i++;)
 	{
-		n << 1;
+		n <<= 1;
 		if (b[i] == '1')
 			n += 1;
 	}
